6bc7714394e68cf14aa1039bfc36f12d4fe0052d
hugues <ghugues@netc.fr>
2020-06-26 15:57:00 +0200

    pkg/hithub/: add license header

--- pkg/github/email.go
+++ pkg/github/email.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, Arveto Ink. All rights reserved.
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file.
+
 package github
 
 import (
